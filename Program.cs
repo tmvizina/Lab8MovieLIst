3325ae400d3039be6e7ca79c5c9050a2f507b03c
tmvizina <tmvizina@gmail.com>
2021-06-07 18:36:59 -0400

    Second Commit

--- Program.cs
+++ Program.cs
@@ -11,10 +11,22 @@ namespace Lab8MovieList
             List<Movie> MovieList = new List<Movie>();
 
             List<string> GenreList = new List<string>();
+
+            List<Movie> SortedList = new List<Movie>();
             GenreList.Add("Animated");
             GenreList.Add("Drama");
             GenreList.Add("Horror");
             GenreList.Add("Science Fiction");
+
+            Console.WriteLine("Please pick a Category ");
+            for (int i = 0; i < GenreList.Count; i++)
+            {
+
+                Console.WriteLine($"{i + 1}). {GenreList[i]}");
+
+            }
+
+
 
             MovieList.Add(new Movie("Toy Story", "Animated"));
             MovieList.Add(new Movie("Wreck It Ralph", "Animated"));
@@ -27,17 +39,86 @@ namespace Lab8MovieList
             MovieList.Add(new Movie("Toy Story", "Science Fiction"));
             MovieList.Add(new Movie("2001: A Space Odyssey", "Science Fiction"));
 
-         
 
-            string input = GetUserInput("Which Genre of Movie would you like to view ? ");
-           
+            Console.WriteLine("Which Genre of Movie would you like to view (1-4)? ");
 
-            for (int i = 0; i < MovieList.Count; i++)
+            bool goOn = true;
+
+            while (goOn == true)
             {
-                if (MovieList[i].Genre==input)
+                try
                 {
-                    Console.WriteLine($"{MovieList[i].Name}");
+                    string genreselected = (Console.ReadLine());
+
+                    int intselected = int.Parse(genreselected);
+
+                    if (intselected < 1 || intselected >= 5)
+                    {
+                        throw new Exception("Out of Bounds please select another number on the list");
+                    }
+
+                    string input = "";
+
+                    if (genreselected == "1")
+                    {
+                        input = "Animated";
+                    }
+                    else if (genreselected == "2")
+                    {
+                        input = "Drama";
+                    }
+                    else if (genreselected == "3")
+                    {
+                        input = "Horror";
+                    }
+                    else if (genreselected == "4")
+                    {
+                        input = "Science Fiction";
+                    }
+
+
+
+                    for (int i = 0; i < MovieList.Count; i++)
+                    {
+
+                        if (MovieList[i].Genre == input)
+                        {
+                            SortedList.Add(MovieList[i]);
+                        }
+
+                    }
+
+
+                    Console.WriteLine("");
+
+                    IEnumerable<Movie> sortMovie =
+
+                        from movie in SortedList
+                        orderby movie.Name
+                        select movie;
+
+                    foreach (Movie movie in sortMovie)
+                    {
+                        Console.WriteLine(movie.Name);
+                    }
+
+                    Console.WriteLine("Would you like to view another Genre? (y/n)");
+                    string go = Console.ReadLine().ToLower();
+                    if (go !="y")
+                    {
+                        goOn = false; 
+                    }
                 }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Please enter valid inputs only"); ;
+
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+
 
             }
 
@@ -45,33 +126,18 @@ namespace Lab8MovieList
 
         public static string GetUserInput(string message)
         {
-            bool goOn = true;
-
-            while (goOn == true)
-            {
-                try
-                {
+            
+                
                     Console.WriteLine(message);
                     string input = Console.ReadLine();
-
-
-                    if (input == "" || input == " ")
-                    {
-                        throw new Exception("Blanks are not a valid input Sorry");
-                    }
-                    else
-                    {
-                        goOn = false;
-                        return input;
-                       
-                    }
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine(e.Message);
-                }
-                }
-            }
+                    return input;
+                
+                
+                
+            
+            
         }
     }
+}
 
+   
