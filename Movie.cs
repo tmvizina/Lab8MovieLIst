cc5d5d3e6908455062a6a43e3b69fb4b89bf397c
tmvizina <tmvizina@gmail.com>
2021-06-07 15:57:12 -0400

    First Commit

---
+++ Movie.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab8MovieList
+{
+    class Movie
+    {
+
+        //This class is a container class for storing Movie Name as well as Genre
+        
+        public string Name { get; set; }
+
+        public string Genre { get; set; }
+
+
+        //This is the constructer method that combines all the data
+        public Movie(string name, string genre)
+        {
+            Name = name;
+            Genre = genre;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Lab8MovieList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Movie> MovieList = new List<Movie>();
+
+            List<string> GenreList = new List<string>();
+            GenreList.Add("Animated");
+            GenreList.Add("Drama");
+            GenreList.Add("Horror");
+            GenreList.Add("Science Fiction");
+
+            MovieList.Add(new Movie("Toy Story", "Animated"));
+            MovieList.Add(new Movie("Wreck It Ralph", "Animated"));
+            MovieList.Add(new Movie("Shrek", "Animated"));
+            MovieList.Add(new Movie("Shawshank Redemption", "Drama"));
+            MovieList.Add(new Movie("Green Mile", "Drama"));
+            MovieList.Add(new Movie("Nightmare on Elm Street", "Horror"));
+            MovieList.Add(new Movie("Friday the 13th", "Horror"));
+            MovieList.Add(new Movie("Interstellar", "Science Fiction"));
+            MovieList.Add(new Movie("Toy Story", "Science Fiction"));
+            MovieList.Add(new Movie("2001: A Space Odyssey", "Science Fiction"));
+
+         
+
+            string input = GetUserInput("Which Genre of Movie would you like to view ? ");
+           
+
+            for (int i = 0; i < MovieList.Count; i++)
+            {
+                if (MovieList[i].Genre==input)
+                {
+                    Console.WriteLine($"{MovieList[i].Name}");
+                }
+
+            }
+
+        }
+
+        public static string GetUserInput(string message)
+        {
+            bool goOn = true;
+
+            while (goOn == true)
+            {
+                try
+                {
+                    Console.WriteLine(message);
+                    string input = Console.ReadLine();
+
+
+                    if (input == "" || input == " ")
+                    {
+                        throw new Exception("Blanks are not a valid input Sorry");
+                    }
+                    else
+                    {
+                        goOn = false;
+                        return input;
+                       
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+                }
+            }
+        }
+    }
+
